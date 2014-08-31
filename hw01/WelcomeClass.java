1a2749db5ff2b0f95b34897988630cc5a0153c80
anika5005 <anc418@lehigh.edu>
2014-08-31 19:22:47 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,29 @@
+/////////////////////////////////////////////////////////////
+//Brian Chen
+//Demo 0
+//Welcome Class Java Program
+//
+//  first compile the program
+//      java WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+    
+//  add a main method
+    public static void main(String[ ] args) {
+        
+//  print the statement with box around it
+    System.out.println("     -----------");
+    System.out.println("     | Welcome |");
+    System.out.println("     -----------");
+    System.out.println("    ^  ^  ^  ^  ^  ^");
+    System.out.println("   / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("  |-A--N--C--4--1--8-|");
+    System.out.println("   \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("    v  v  v  v  v  v");
+    System.out.println("This April I went on a hiking trip in the Nepalese");
+    System.out.println("Himalayas");
+    }
+}
