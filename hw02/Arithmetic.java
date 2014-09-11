c9a3064eebfb7c19f0e2f361b1dee0c0207da894
anika5005 <anc418@lehigh.edu>
2014-09-11 00:41:05 +0000

    updated Arithmetic

--- hw02/Arithmetic.java
+++ hw02/Arithmetic.java
@@ -40,7 +40,8 @@ public static void main(String[ ] args) {
     double salesTaxSocks; //define sales tax for the socks
     double salesTaxGlasses; //define sales tax for the glasses
     double salesTaxEnvelopes; //define sales tax for the envelopes
-    
+    double totalAmountWithTax; //define total amount paid, including tax
+   
 //  compute the total costs for values (without tax)
     totalSockCost$=nSocks*sockCost$;    //define total cost for socks
     totalGlassCost$=nGlasses*glassCost$;    //define total cost for drinking glasses
@@ -49,18 +50,39 @@ public static void main(String[ ] args) {
     salesTaxSocks=0.06*totalSockCost$;  //define socks sale tax by finding 6% of the total sock cost
     salesTaxGlasses=0.06*totalGlassCost$;   //define glasses sale tax by finding 6% of the total glasses cost
     salesTaxEnvelopes=0.06*totalEnvelopeCost$;  //define envelopes sale tax by finding 6% of the total glasses cost
+    totalAmountWithTax = totalPurchaseCost$ + (0.06*totalPurchaseCost$); //define total amount paid with tax by adding 6% of total purchase cost to the total purchase cost 
+ 
+//  convert sales tax of socks to 2 decimal places
+    salesTaxSocks =  salesTaxSocks*100; //multiply by 100 to shift decimal places to the right by 2 numbers
+    salesTaxSocks = (int) salesTaxSocks; //convert to int 
+    salesTaxSocks = salesTaxSocks/100; //divide by 100 to get sales tax with only 2 d.p.
     
+//  convert sales tax of drinking glasses to 2 decimal places
+    salesTaxGlasses = salesTaxGlasses*100;
+    salesTaxGlasses = (int) salesTaxGlasses;
+    salesTaxGlasses = salesTaxGlasses/100;
     
+//  convert sales tax of envelopes to 2 decimal places
+    salesTaxEnvelopes = salesTaxEnvelopes*100;
+    salesTaxEnvelopes = (int) salesTaxEnvelopes;
+    salesTaxEnvelopes = salesTaxEnvelopes/100;
+    
+//  convert total amount paid for transaction to 2 decimal places
+    totalAmountWithTax = totalAmountWithTax*100;
+    totalAmountWithTax = (int) totalAmountWithTax;
+    totalAmountWithTax = totalAmountWithTax/100;
+    
+ 
 //  Calculate the total cost of each type of item and the sales tax for each cost
         System.out.println ("The total cost of socks was " + (nSocks*sockCost$) 
-        + " dollars, and the sales tax was " + (0.06*totalSockCost$) + 
+        + " dollars, and the sales tax was " + (salesTaxSocks) + 
         " dollars.");
         System.out.println ("The total cost of drinking glasses was " + 
         (nGlasses*glassCost$ ) + " dollars, and the sales tax was " + 
-        (0.06*totalGlassCost$) + " dollars." );
+        (salesTaxGlasses) + " dollars." );
         System.out.println ("The total cost of envelopes was " + 
          (nEnvelopes*envelopeCost$) + " dollars, and the sales tax was "  + 
-        (0.06*totalEnvelopeCost$) + " dollars.");
+        (salesTaxEnvelopes) + " dollars.");
         
 //  Calculate the total cost of the purchases, without tax
 //  add all total costs
@@ -72,9 +94,7 @@ public static void main(String[ ] args) {
 //  add six percent of total cost for purchase, to the total cost for the purchase
         System.out.println 
         ("The total amount paid for the transaction,including sales tax, was " 
-         +  ((totalPurchaseCost$) + (0.06*totalPurchaseCost$)) + " dollars.");
+         +  (totalAmountWithTax) + " dollars.");
       
-       
-        
 }   //end of main method
 }   //end of class
